91ca8184c2ab5a1e44112b674182ab0147b768c7
Sarthak Bhatt <sarthakbhatt1407@gmail.com>
2024-05-06 17:22:54 +0530

    updated login

--- src/components/UserPanel/Form.jsx
+++ src/components/UserPanel/Form.jsx
@@ -249,20 +249,18 @@ const Form = () => {
   };
   const fileProps = {
     beforeUpload: (file) => {
-      const isValid =
-        file.type === "audio/wav" ||
-        file.type === "audio/mp3" ||
-        // file.type === "audio/mpeg" ||
-        // file.type === "audio/aac" ||
-        // file.type === "audio/flac" ||
-        // file.type === "audio/alac" ||
-        // file.type === "audio/wma" ||
-        // file.type === "audio/aiff" ||
-        // file.type === "video/mp4" ||
-        // file.type === "video/x-msvideo" ||
-        // file.type === "video/x-ms-wmv" ||
-        // file.type === "video/x-flv" ||
-        // file.type === "video/quicktime";
+      const isValid = file.type === "audio/wav" || file.type === "audio/mp3";
+      // file.type === "audio/mpeg" ||
+      // file.type === "audio/aac" ||
+      // file.type === "audio/flac" ||
+      // file.type === "audio/alac" ||
+      // file.type === "audio/wma" ||
+      // file.type === "audio/aiff" ||
+      // file.type === "video/mp4" ||
+      // file.type === "video/x-msvideo" ||
+      // file.type === "video/x-ms-wmv" ||
+      // file.type === "video/x-flv" ||
+      // file.type === "video/quicktime";
 
       if (!isValid) {
         message.error(`Upload valid audio or video file!`);
